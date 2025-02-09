3abb14426cbe63d05953d054dc381afee83a6b7c
GarbageCode1984 <rkqlwl1984@gmail.com>
2025-02-09 19:04:42 +0900

    thumbnail

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,5 +1,23 @@
-function Thumbnail() {
-    return <div>Thumbnail</div>;
+import styled from "styled-components";
+
+interface image {
+    src: string;
+    alt: string;
 }
+
+function Thumbnail() {
+    const images: image[] = [
+        { src: "/assets/images/test1.jpg", alt: "test1" },
+        { src: "/assets/images/test2.jpg", alt: "test2" },
+        { src: "/assets/images/test3.jpg", alt: "test3" },
+        { src: "/assets/images/test4.jpg", alt: "test4" },
+    ];
+
+    return <Container></Container>;
+}
+
+const Container = styled.div`
+    display: grid;
+`;
 
 export default Thumbnail;
