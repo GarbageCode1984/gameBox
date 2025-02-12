94c54cd1d0e829721cdd7adeaf63de7fecdcda18
GarbageCode1984 <rkqlwl1984@gmail.com>
2025-02-13 05:18:36 +0900

    4x4 board

--- src/pages/MemoryCard/MemoryGame .tsx
+++ src/pages/MemoryCard/MemoryGame .tsx
@@ -5,6 +5,10 @@ import card1 from "../../assets/MemoryCard/Card/card1.png";
 import card2 from "../../assets/MemoryCard/Card/card2.png";
 import card3 from "../../assets/MemoryCard/Card/card3.png";
 import card4 from "../../assets/MemoryCard/Card/card4.png";
+import card5 from "../../assets/MemoryCard/Card/card5.png";
+import card6 from "../../assets/MemoryCard/Card/card6.png";
+import card7 from "../../assets/MemoryCard/Card/card7.png";
+import card8 from "../../assets/MemoryCard/Card/card8.png";
 
 interface Image {
     id: number;
@@ -15,6 +19,10 @@ const images: Image[] = [
     { id: 2, src: card2 },
     { id: 3, src: card3 },
     { id: 4, src: card4 },
+    { id: 5, src: card5 },
+    { id: 6, src: card6 },
+    { id: 7, src: card7 },
+    { id: 8, src: card8 },
 ];
 
 const generateCards = (): Image[] => {
@@ -50,24 +58,32 @@ function MemoryGame() {
     };
 
     return (
-        <Board>
-            {cards.map((card, index) => (
-                <Card
-                    key={card.id}
-                    id={index}
-                    image={card.src}
-                    isFlipped={flippedIndices.includes(index) || matchedIndices.includes(index)}
-                    onClick={handleCardClick}
-                />
-            ))}
-        </Board>
+        <Container>
+            <Board>
+                {cards.map((card, index) => (
+                    <Card
+                        key={card.id}
+                        id={index}
+                        image={card.src}
+                        isFlipped={flippedIndices.includes(index) || matchedIndices.includes(index)}
+                        onClick={handleCardClick}
+                    />
+                ))}
+            </Board>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
 
 const Board = styled.div`
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    gap: 10px;
 `;
 
 export default MemoryGame;
